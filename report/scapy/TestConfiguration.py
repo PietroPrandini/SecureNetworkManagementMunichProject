1461e4a9b54737d4079dd5379581072c56432fe9
Pietro Prandini <prandinip@gmail.com>
2018-12-27 16:30:09 +0100

    test

--- report/scapy/TestConfiguration.py
+++ report/scapy/TestConfiguration.py
@@ -1,13 +1,68 @@
 #! /usr/bin/env python
 from scapy.all import *
 
-# Availability of each subnets
-arping("192.168.40.0/24")
-arping("192.168.42.0/24")
-arping("192.168.44.0/24")
-arping("192.168.48.0/24")
-arping("192.168.50.0/24")
-arping("192.168.55.0/24")
-arping("192.168.62.0/24")
+# Availability of each device
+ans,unans = arping("192.168.40.50")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.40.11")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.42.1")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.42.12")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.44.3")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.44.4")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.48.6")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.48.9")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.50.1")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.50.7")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.55.8")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.55.5")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.62.10")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.62.60")
+ans.summary()
+unans.summary()
+
+ans,unans = arping("192.168.62.70")
+ans.summary()
+unans.summary()
+
 # Testing the NAT
 arping("www.google.com")
+ans.summary()
+unans.summary()

9ae814d4abac03c86050494f73f471a65416f192
Pietro Prandini <prandinip@gmail.com>
2018-12-27 14:07:53 +0100

    Devices configuration

--- report/scapy/TestConfiguration.py
+++ report/scapy/TestConfiguration.py
@@ -1,2 +1,13 @@
 #! /usr/bin/env python
 from scapy.all import *
+
+# Availability of each subnets
+arping("192.168.40.0/24")
+arping("192.168.42.0/24")
+arping("192.168.44.0/24")
+arping("192.168.48.0/24")
+arping("192.168.50.0/24")
+arping("192.168.55.0/24")
+arping("192.168.62.0/24")
+# Testing the NAT
+arping("www.google.com")

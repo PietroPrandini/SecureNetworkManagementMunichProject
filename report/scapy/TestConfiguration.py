539c8350d6cabb194c8f2f1fb1d32d8a954c8280
Pietro Prandini <prandinip@gmail.com>
2018-12-27 23:37:29 +0100

    Test configuration

--- report/scapy/TestConfiguration.py
+++ report/scapy/TestConfiguration.py
@@ -1,142 +1,63 @@
 #! /usr/bin/env python
 from scapy.all import *
 
+def check_availability(target):
+    ans,unans = sr(IP(dst=target)/ICMP())
+    if ans:
+        print(target + ' is reachable, summary:')
+        ans.summary()
+        return ans,unans
+    else:
+        print(target + ' is not reachable, summary:')
+        unans.summary()
+        return ans,unans
+
 # Availability of each device
 target = "192.168.40.50"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.40.11"
-ans.summary()
+check_availability(target)
 
 target = "192.168.42.1"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.42.12"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.44.3"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.44.4"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.48.6"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.48.9"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
-target = "192.168.50.1"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+target = "192.168.50.2"
+check_availability(target)
 
 target = "192.168.50.7"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.55.8"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.55.5"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.62.10"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.62.60"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 target = "192.168.62.70"
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+check_availability(target)
 
 # Testing the NAT
-arping("www.google.com")
-ans,unans = arping(target)
-if ans:
-    print(target + ' is reachable, summary:')
-    ans.summary()
-else:
-    print(target + ' is not reachable, summary:')
-    unans.summary()
+target = "www.google.com"
+check_availability(target)

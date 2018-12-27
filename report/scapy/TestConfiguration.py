642186454ed9f6d4803758fe593ee10e4a6e581c
Pietro Prandini <prandinip@gmail.com>
2018-12-27 22:46:02 +0100

    All routers reachable from PC1 and PC2

--- report/scapy/TestConfiguration.py
+++ report/scapy/TestConfiguration.py
@@ -2,67 +2,141 @@
 from scapy.all import *
 
 # Availability of each device
-ans,unans = arping("192.168.40.50")
-ans.summary()
-unans.summary()
+target = "192.168.40.50"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.40.11")
+target = "192.168.40.11"
 ans.summary()
-unans.summary()
 
-ans,unans = arping("192.168.42.1")
-ans.summary()
-unans.summary()
+target = "192.168.42.1"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.42.12")
-ans.summary()
-unans.summary()
+target = "192.168.42.12"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.44.3")
-ans.summary()
-unans.summary()
+target = "192.168.44.3"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.44.4")
-ans.summary()
-unans.summary()
+target = "192.168.44.4"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.48.6")
-ans.summary()
-unans.summary()
+target = "192.168.48.6"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.48.9")
-ans.summary()
-unans.summary()
+target = "192.168.48.9"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.50.1")
-ans.summary()
-unans.summary()
+target = "192.168.50.1"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.50.7")
-ans.summary()
-unans.summary()
+target = "192.168.50.7"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.55.8")
-ans.summary()
-unans.summary()
+target = "192.168.55.8"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.55.5")
-ans.summary()
-unans.summary()
+target = "192.168.55.5"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.62.10")
-ans.summary()
-unans.summary()
+target = "192.168.62.10"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.62.60")
-ans.summary()
-unans.summary()
+target = "192.168.62.60"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
-ans,unans = arping("192.168.62.70")
-ans.summary()
-unans.summary()
+target = "192.168.62.70"
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()
 
 # Testing the NAT
 arping("www.google.com")
-ans.summary()
-unans.summary()
+ans,unans = arping(target)
+if ans:
+    print(target + ' is reachable, summary:')
+    ans.summary()
+else:
+    print(target + ' is not reachable, summary:')
+    unans.summary()

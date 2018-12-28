df3e650dc3ea6982123e0d65733c9f11de45ecb4
Pietro Prandini <prandinip@gmail.com>
2018-12-28 13:27:03 +0100

    IPSpoofing works started

--- report/scapy/IPSpoofing.py
+++ report/scapy/IPSpoofing.py
@@ -1,2 +1,13 @@
 #! /usr/bin/env python
 from scapy.all import *
+
+# Preparing the packet for Spoofing
+attacker = "192.168.102.102"
+victim = "192.168.62.60"
+spoof = IP(dst=victim, src=attacker)/ICMP()/"spoof"
+
+# Sending the packets
+send(spoof, inter=0.005)
+send(spoof, inter=0.010)
+send(spoof, inter=0.020)
+send(spoof, inter=0.050)

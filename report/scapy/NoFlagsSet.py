77a8d63f11b12f0427190e28e21c854556bbd06b
Pietro Prandini <prandinip@gmail.com>
2018-12-28 20:07:44 +0100

    No flags set

--- report/scapy/NoFlagsSet.py
+++ report/scapy/NoFlagsSet.py
@@ -1,2 +1,9 @@
 #! /usr/bin/env python
 from scapy.all import *
+
+# Preparing the packet with no flags set
+victim = "192.168.62.60"
+packet_without_flags = IP(dst=victim)/TCP(flags="")
+
+# Sending the packet
+send(packet_without_flags)

